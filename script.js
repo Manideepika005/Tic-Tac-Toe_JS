cb8b86003a396aceca832ffef4b9043f30cc4314
Manideepika005 <smanideepika005@gmail.com>
2025-08-17 12:42:33 +0530

    made new and reset game buttons work

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ const winPatterns = [
     [6,7,8]
 ];
 
+const resetGame = () =>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
 boxes.forEach((box) => {
     box.addEventListener("click", ()=>{
         console.log("box was clicked");
@@ -33,10 +39,26 @@ boxes.forEach((box) => {
         checkWinner();
     });
 });
+
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
 const showWinner = (winner) => {
     msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
-}
+    disableBoxes();
+};
+
 const checkWinner = () =>{
     for(let pattern of winPatterns){
         let pos1Val = boxes[pattern[0]].innerText;
@@ -50,4 +72,9 @@ const checkWinner = () =>{
             }
         }
     }
-}
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetbtn.addEventListener("click", resetGame);
+
+
