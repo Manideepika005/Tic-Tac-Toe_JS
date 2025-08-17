7952ddd2094b50e1b03921ba3c7c6ee29af7b7b8
Manideepika005 <smanideepika005@gmail.com>
2025-08-17 10:57:01 +0530

    added code for showing winner

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetbtn = document.querySelector("#reset");
+let newGameBtn = document.querySelector("#new");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -30,7 +33,10 @@ boxes.forEach((box) => {
         checkWinner();
     });
 });
-
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hode");
+}
 const checkWinner = () =>{
     for(let pattern of winPatterns){
         let pos1Val = boxes[pattern[0]].innerText;
@@ -40,6 +46,7 @@ const checkWinner = () =>{
         if(pos1Val != "" && pos2Val != "" && pos3Val !=""){
             if(pos1Val === pos2Val && pos2Val === pos3Val){
                 console.log("winner" , pos1Val);
+                showWinner();
             }
         }
     }
